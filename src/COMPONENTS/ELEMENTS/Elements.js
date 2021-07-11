2b2b3ee08134515c30e5ebefdf6a7eb57d6aeeee
Akarsh Barar <barar.akarsh8@gmail.com>
2021-07-12 01:44:48 +0530

    Added Elements section

--- src/COMPONENTS/ELEMENTS/Elements.js
+++ src/COMPONENTS/ELEMENTS/Elements.js
@@ -1,7 +1,43 @@
 
-import React from 'react'
+import React, {useState} from 'react'
+
 import './Elements.css'
 function Elements() {
+
+    const [selectedItems, setSelectedItems] = useState([]);
+    var selectionItems=[
+        "Acknowledgement",
+        "API Refrence",
+        "Apprendix",
+        "Authors",
+        "Badges",
+        "Color Refrences",
+        "Contributing",
+        "Demo",
+        "Deployment",
+        "Documentation",
+        "Environment Variable",
+        "FAQ",
+        "Features",
+        "Feedback",
+        "Github",
+        "Installation",
+        "License",
+        "Lessons",
+        "Roadmap",
+        "Support",
+        "Title and Description"
+    ];
+
+    function addToSelectedItems(item){
+        var newSelectedItems = selectedItems.slice();
+        newSelectedItems.push(item);
+        newSelectedItems.reduce(function(a,b){
+            if (a.indexOf(b) < 0 ) a.push(b);
+            return a;
+        },[]);
+        setSelectedItems(newSelectedItems);
+    }
     return (
         <div className="elements">
             <div className="elem__heading">
@@ -11,23 +47,25 @@ function Elements() {
                 <div className="elem__section">
                     <span className="elem__title">Selected Elements will appear here</span>
                     <div className="elem__items">
-                        <div className="elem__item">
-                            Item 1
-                        </div>
-                        <div className="elem__item">
-                            Item 2
-                        </div>
+                        {
+                            selectedItems.map(function(item){  
+                                return <div className="elem__item" >{item}</div>
+                            })
+                        }                            
+                      
                     </div>
                 </div>
                 <div className="elem__section">
                     <span className="elem__title">Click on Elements to add to your page</span>
                     <div className="elem__items">
-                        <div className="elem__item">
-                            Item 1
-                        </div>
-                        <div className="elem__item">
-                            Item 2
-                        </div>
+                        {
+                            selectionItems.map(function(item, index){
+                                return <div className="elem__item" key={index} onClick={()=>addToSelectedItems(`${item}`)}>
+                                    {item}
+                                </div>
+                            })
+                        }
+                     
                     </div>
                 </div>
 
--- src/PAGES/EDITOR/Editor.js
+++ src/PAGES/EDITOR/Editor.js
@@ -3,11 +3,12 @@ import Elements from '../../COMPONENTS/ELEMENTS/Elements'
 import MainEditor from '../../COMPONENTS/MARKDOWNEDITOR/MainEditor';
 import Preview from '../../COMPONENTS/PREVIEW/Preview';
 import './Editor.css'
+import { Link} from 'react-router-dom'
 function Editor() {
     return (
         <div className="editor">
             <nav>
-                <div className="logo">ReadME_Creator</div>
+                <div className="logo"><Link to="/"> ReadME_Creator </Link> </div>
                 <button className="btn btn-primary" onClick={() => {} }>Download</button>
             </nav>
             <div className="editor__content">
